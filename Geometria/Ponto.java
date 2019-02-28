eb46a8ecfdb26fdc6a04bcae67f7bcef48d3d440
Henrique Hänsch Stepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2019-02-28 00:19:18 -0300

    Exercício 6, sem conclusão

---
+++ Geometria/Ponto.java
@@ -0,0 +1,15 @@
+class Ponto{
+	private Double x;
+	private Double y;
+	
+	public Ponto(Double x, Double y) {
+		this.x = x; 
+		this.y = y;
+	}
+
+
+	public String toString() {
+		return "Ponto - x " + this.x + " y: " + this.y;
+	
+	}
+}
